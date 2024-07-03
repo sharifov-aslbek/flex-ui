ba1eb43cfa629242fe03ab01a40430a258d12ae1
Sharifov Aslbek <148238968+sharifov-aslbek@users.noreply.github.com>
2024-07-03 12:17:48 +0500

    added fixed err

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let icon = document.querySelector('.head .fa-bars')
-let xmark = document.querySelector('.head .bg-black .mobile-menu .logo .fa-xmark')
-let bgBlack = document.querySelector('.head .bg-black')
-let mobileMenu = document.querySelector('.head .bg-black .mobile-menu')
+let icon = document.querySelector('.icon .fa-bars')
+let xmark = document.querySelector('.bg-black .mobile-menu .logo .fa-xmark')
+let bgBlack = document.querySelector('.bg-black')
+let mobileMenu = document.querySelector('.bg-black .mobile-menu')
 icon.addEventListener("click" , () => {
    bgBlack.classList.add('active')
    mobileMenu.classList.add('active')
